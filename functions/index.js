cad80a850284ee5ac621feeac9ac4037c722c9da
和田健嗣 <ponporopen@gmail.com>
2020-03-19 20:52:21 +0900

    不要な記述を削除
    functions/index

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,3 @@
-// import * as functions from 'firebase-functions'
-// import next from 'next'
-//
-// // const app = next({
-// //   dev: false,
-// //   conf: {distDir: 'dist'},
-// // })
-// // const handle = app.getRequestHandler()
-// //
-// // exports.nextApp = functions.https.onRequest((req, res) => {
-// //   console.log('File: ' + req.originalUrl)
-// //   return app.prepare().then(() => handle(req, res))
-// // })
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   response.send("Hello from functions!!!!")
-// })
-
 const functions = require('firebase-functions')
 const next = require('next')
 
