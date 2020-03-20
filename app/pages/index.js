a2fcad971f98fecd2653b65f3ccc46128496e35c
和田健嗣 <ponporopen@gmail.com>
2020-03-20 19:16:07 +0900

    indexページ実装　掲示板のような

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,15 +1,79 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 
+import {messagesRef} from "../plugins/firebase"
+
 const Index = () => {
+  const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+  const [newUsername, setNewUsername] = useState('')
+
+  messagesRef.onSnapshot(doc => {
+    if (doc.docs.length !== messages.length) getMessages()
+  })
+
+  useEffect(() => {
+    getMessages()
+  }, [])
+
+  const onChangeInput = (e, setState) => {
+    setState(e.target.value)
+  }
+
+  const setMessage = () => {
+    messagesRef.doc().set({
+      datetime: new Date(),
+      user: newUsername,
+      content: newMessage
+    })
+    setNewMessage('')
+    setNewUsername('')
+  }
+
+  const getMessages = () => {
+    messagesRef.orderBy('datetime', 'desc').get()
+      .then(snapshot => {
+        let tmp = []
+        snapshot.forEach(doc => tmp.push({id: doc.id, ...doc.data()}))
+        setMessages(tmp)
+      })
+      .catch(err => {
+        console.log('Error getting documents', err)
+      })
+  }
+
+  const renderMessages = () => (
+    <div>
+      <h3>メッセージ一覧</h3>
+      {messages.map(m => (
+        <div key={m.id}>
+          <p>投稿者: {m.user} - {new Date(m.datetime.seconds * 1000).toLocaleString('ja-JP')}</p>
+          <p>{m.content}</p>
+          <hr/>
+        </div>
+      ))}
+    </div>
+  )
+
   return (
     <>
-      <div>
+      <h1>
         Index
-      </div>
+      </h1>
       <Link href="/about">
-        <a>about</a>
+        <a>to about page</a>
       </Link>
+      <div style={{display: 'flex', flexDirection: 'column', width: '30%', marginBottom: '50px'}}>
+        <h3>メッセージを投稿する</h3>
+        <label htmlFor="name">なまえ</label>
+        <input name="name" value={newUsername} style={{marginBottom: '20px'}}
+               onChange={(e) => onChangeInput(e, setNewUsername)}/>
+        <label htmlFor="message">本文</label>
+        <textarea name="message" value={newMessage} style={{marginBottom: '20px'}}
+                  onChange={(e) => onChangeInput(e, setNewMessage)}/>
+        <button onClick={setMessage} style={{width: '50px'}}>投稿</button>
+      </div>
+      {renderMessages()}
     </>
   )
 }
